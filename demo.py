e03568d15a7e306433c7550ff16c1d0c9d0a59a4
guangxush <guangxush@163.com>
2019-10-08 09:43:33 +0800

    baseline

--- demo.py
+++ demo.py
@@ -1,17 +1,13 @@
 import numpy as np
-
 np.random.seed(1337)  # for reproducibility
-
-from keras.models import Model  # 泛型模型
+from keras.models import Model
 from keras.layers import Dense, Input
-import matplotlib.pyplot as plt
 import pandas as pd
 
 
 def demo():
     # 数据预处理
     train_dataframe = pd.read_csv('./data/iris.data', header=0)
-    # print(train_dataframe)
     train_dataset = train_dataframe.values
     total_count = 150
     train_level = int(total_count*0.7)
@@ -23,33 +19,33 @@ def demo():
     print(x_train.shape)
     print(x_test.shape)
 
-    # 压缩特征维度至2维
-    encoding_dim = 2
-
+    # 压缩特征维度至5维
+    encoding_dim = 5
     # this is our input placeholder
-    input_img = Input(shape=(4,), name="input_img")
+    data_input = Input(shape=(4,), name="data_input")
     decoder_input = Input(shape=(encoding_dim,), name="decoder_input")
 
     # 编码层
-    encoded = Dense(128, activation='relu')(input_img)
+    encoded = Dense(128, activation='relu')(data_input)
     encoded = Dense(64, activation='relu')(encoded)
     encoded = Dense(10, activation='relu')(encoded)
-    encoder_output = Dense(encoding_dim)(encoded)
+    encoder_input = Dense(encoding_dim)(encoded)
 
     # 解码层
-    decoded = Dense(10, activation='relu')(encoder_output)
+    decoded = Dense(10, activation='relu')(encoder_input)
     decoded = Dense(64, activation='relu')(decoded)
     decoded = Dense(128, activation='relu')(decoded)
     decoded = Dense(4, activation='tanh')(decoded)
 
     # 构建自编码模型
-    autoencoder = Model(inputs=input_img, outputs=decoded)
+    autoencoder = Model(inputs=data_input, outputs=decoded)
 
     # 构建编码模型
-    encoder = Model(inputs=input_img, outputs=encoder_output)
+    encoder = Model(inputs=data_input, outputs=encoder_input)
 
     # 构建解码模型
-    decoder = Model(inputs=decoder_input, outputs=decoded)
+    decoder_layer = autoencoder.layers[-1]
+    decoder = Model(inputs=decoder_input, outputs=decoder_layer(decoder_input))
 
     # compile autoencoder
     autoencoder.compile(optimizer='adam', loss='mse')
